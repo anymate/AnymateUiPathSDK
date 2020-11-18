37ca381c3ca3f6c46a249cff10b0e69fd74b9782
Simon <simon@anymate.io>
2020-11-18 10:17:02 +0100

    updated helpers

---
+++ Anymate.UiPath/Helpers/GetCreateTaskDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System.Activities;
+using System.ComponentModel;
+using Newtonsoft.Json.Linq;
+using System.DateTime;
+
+namespace Anymate.UiPath.Helpers
+{
+    public class GetCreateTaskJsonObject : CodeActivity
+    {
+
+        [Category("Input - Optional")]
+        [DefaultValue(null)]
+        public InArgument<string> Comment { get; set; }
+
+        [Category("Input - Optional")]
+        [DefaultValue(null)]
+        public InArgument<DateTime?> ActivationDate { get; set; }
+
+        [Category("Output - FlowControl")]
+        public OutArgument<Dictionary<string,string>> Dict { get; set; }
+
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var dict = new Dictionary<string,string>();
+            var newNote = Comment.Get(context);
+            if(!string.IsNullOrWhiteSpace(newNote))
+            {
+                dict.Add("comment", newNote);
+            }
+
+            var activationDate = ActivationDate.Get(context);
+            if(activationDate != null)
+            {
+                dict.Add("activationDate", activationDate.ToString("s"))
+            }
+
+            Dict.Set(context, dict);
+        }
+    }
+}
---
+++ Anymate.UiPath/Helpers/GetUpdateTaskDictionary.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Activities;
+using System.ComponentModel;
+
+namespace Anymate.UiPath.Helpers
+{
+    public class GetUpdateTaskJsonObject : CodeActivity
+    {
+
+        [Category("Input")]
+        [DefaultValue(null)]
+        [RequiredArgument]
+        public InArgument<long> TaskId { get; set; }
+
+        [Category("Input - Optional")]
+        public InArgument<string> Comment { get; set; }
+
+        [Category("Output - FlowControl")]
+        public OutArgument<Dictionary<string,string>> Dict { get; set; }
+
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            var dict = new Dictionary<string,string>();
+            var taskId = TaskId.Get(context);
+            dict.Add(nameof(taskId), taskId);
+
+            var newNote = Comment.Get(context);
+            if(!string.IsNullOrWhiteSpace(newNote))
+            {
+                dict.Add("comment", newNote);
+            }
+
+            Dict.Set(context, dict);
+        }
+    }
+}
