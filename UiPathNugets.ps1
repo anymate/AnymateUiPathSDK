8dd92a1159f173b00d7f55844c8897ae0ab7d2b1
Simon Faltum <simon@anymate.io>
2020-11-18 11:30:53 +0100

    Version 1.0.0

--- UiPathNugets.ps1
+++ UiPathNugets.ps1
@@ -1,5 +1,2 @@
 rm *.nupkg
-.\nuget.exe pack .\Anymate.UiPath.API\Anymate.UiPath.API.csproj -IncludeReferencedProjects 
-.\nuget.exe pack .\Anymate.UiPath.Auth\Anymate.UiPath.Auth -IncludeReferencedProjects 
-.\nuget.exe pack .\Anymate.UiPath.Helpers\Anymate.UiPath.Helpers.csproj -IncludeReferencedProjects 
-
+.\nuget.exe pack .\Anymate.UiPath\Anymate.UiPath.csproj -IncludeReferencedProjects 
